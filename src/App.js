07b3c6351ac0e4c011070972bdbb8f1ee6373aca
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2020-08-16 19:47:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import Chart from "chart.js";
+import axios from "axios";
+import globalTemps from "./GLB.Ts+dSST.csv";
+import northernTemps from "./NH.Ts+dSST.csv";
+import southernTemps from "./SH.Ts+dSST.csv";
+import test from "./test.csv";
+
+import "./App.css";
+
+const API_ENDPOINT = "";
+
+
 
 function App() {
+
+
+  const getISS = useCallback(async () => {
+     const response = await axios.get(API_ENDPOINT); 
+     console.log(response);
+
+  }, []);
+
+
+
+  useEffect(() => {
+    getISS();
+  }, [getISS]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
